5e9ccd3d1b8c6efed292a1964a9e84c047546e35
Klaudia Golebiowska <k.golebiowska.aim@gmail.com>
2021-01-04 06:55:54 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🥳 CORRECT!🥳🥳🥳';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
