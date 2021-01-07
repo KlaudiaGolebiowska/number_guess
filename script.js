ef03687d4ff0597bf8303d2d5bae97aca86e6d89
Klaudia Golebiowska <k.golebiowska.aim@gmail.com>
2021-01-07 03:46:31 +0100

    Secret number hide

--- script.js
+++ script.js
@@ -9,8 +9,6 @@
 // console.log(document.querySelector('guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -27,6 +25,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🥳 CORRECT!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   }
 
   // When number given is too high
