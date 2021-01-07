e8f9ec706b95d410476309b924db08c92bc7207c
Klaudia Golebiowska <k.golebiowska.aim@gmail.com>
2021-01-07 03:27:58 +0100

    Background color changed when player win/lost

--- script.js
+++ script.js
@@ -11,17 +11,51 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no number given
   if (!guess) {
     document.querySelector('.message').textContent = ' 🚫 No number!';
-  } else if (guess === secretNumber) {
+  }
+
+  // When player win the game
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 CORRECT!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too high!';
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too low!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+
+  // When number given is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '🤦 Ups, you lost my simple game...';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#FB3640';
+      document.querySelector('.number').style.width = '30rem';
+    }
+  }
+
+  //When number given is too small
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '🤦  Oh no, you lost my simple game...';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#FB3640';
+      document.querySelector('.number').style.width = '30rem';
+    }
   }
 });
