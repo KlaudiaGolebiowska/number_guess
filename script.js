41414bc54ebe72f83f40a61534eef1ba55e287c5
Klaudia Golebiowska <k.golebiowska.aim@gmail.com>
2021-01-07 02:57:09 +0100

    Too low and too high added

--- script.js
+++ script.js
@@ -8,14 +8,20 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('guess').value);
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Numbe(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = ' 🚫 No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳 CORRECT!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too low!';
   }
 });
